3c9a95bfbcf65ebbd4f5281899b3919d2f333f4f
Adl Gamaladdin <gamaladdinadl@gmail.com>
2025-05-11 21:55:09 -0700

    Boggle

--- boggle.cpp
+++ boggle.cpp
@@ -93,7 +93,23 @@ std::set<std::string> boggle(const std::set<std::string>& dict, const std::set<s
 
 bool boggleHelper(const std::set<std::string>& dict, const std::set<std::string>& prefix, const std::vector<std::vector<char> >& board, 
 								   std::string word, std::set<std::string>& result, unsigned int r, unsigned int c, int dr, int dc)
-{
-//add your solution here!
+	{
+	//add your solution here!
+		unsigned int n = board.size();
+		if (r >= n || c >= n) return false;
+
+  		word += board[r][c];
+
+  		if (prefix.find(word) == prefix.end() && dict.find(word) == dict.end()) return false;
+
+  		bool continueSearch = boggleHelper(dict, prefix, board, word, result, r + dr, c + dc, dr, dc);
+
+  		if (!continueSearch && dict.find(word) != dict.end()) {
+      		result.insert(word);
+      		return true;
+  		}
+
+  	return false;
+
 
 }
